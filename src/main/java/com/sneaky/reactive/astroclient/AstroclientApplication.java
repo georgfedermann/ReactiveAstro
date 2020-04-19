647a1c0c49a2a50bc516b9cbf4406a5fdc3283a2
Georg Federmann <georg.federmann@gmail.com>
2020-04-19 10:05:01 +0200

    initial commit

---
+++ src/main/java/com/sneaky/reactive/astroclient/AstroclientApplication.java
@@ -0,0 +1,13 @@
+package com.sneaky.reactive.astroclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstroclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sneaky/reactive/astroclient/AstroclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneaky.reactive.astroclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
