29c7ab38fb713baaf2ae12511dcfef09d9809345
Georg Federmann <georg.federmann@gmail.com>
2020-04-19 11:35:29 +0200

    integration test checks

--- src/test/java/com/sneaky/reactive/astroclient/service/JokeServiceTest.java
+++ src/test/java/com/sneaky/reactive/astroclient/service/JokeServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -21,6 +23,10 @@ class JokeServiceTest {
     void getJokeTest() {
         String joke = service.getJoke("Paul", "Deitel");
         System.out.println(joke);
+        assertAll(
+                () -> assertThat(joke).contains("Paul"),
+                () -> assertThat(joke).contains("Deitel")
+        );
     }
 
 }
